692d641642fa9eda497105082c7384160b2446a0
Wang <henanwan@3c22fb3450bd.ant.amazon.com>
2021-07-15 15:55:14 +0800

    add back all files

--- ObjectNav/run_astar_planning.py
+++ ObjectNav/run_astar_planning.py
@@ -240,16 +240,16 @@ atexit.register(pool.close)
 
 
 files = ['2azQ1b91cZZ.json',
-        #  '8194nk5LbLH.json',
-        #  'EU6Fwq7SyZv.json',
-        #  'oLBMNvg9in8.json',
-        #  'pLe4wQe7qrG.json',
-        #  'QUCTc6BB5sX.json',
-        #  'TbHJrupSAjP.json',
-        #  'X7HyMhZNoso.json',
-        #  'x8F5xyUWy9e.json',
-        #  'Z6MFQCViBuw.json',
-        #  'zsNo4HB9uLZ.json',
+         '8194nk5LbLH.json',
+         'EU6Fwq7SyZv.json',
+         'oLBMNvg9in8.json',
+         'pLe4wQe7qrG.json',
+         'QUCTc6BB5sX.json',
+         'TbHJrupSAjP.json',
+         'X7HyMhZNoso.json',
+         'x8F5xyUWy9e.json',
+         'Z6MFQCViBuw.json',
+         'zsNo4HB9uLZ.json',
          ]
 
 outputs = {}
